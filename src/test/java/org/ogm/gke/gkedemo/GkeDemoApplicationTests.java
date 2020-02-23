aa0cc52f5e449418fcedf09552a65a9f2306cff0
Oscar González <oscar.gonzalezmartinez@gmail.com>
2020-02-23 17:41:26 +0100

    first commit

---
+++ src/test/java/org/ogm/gke/gkedemo/GkeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ogm.gke.gkedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
