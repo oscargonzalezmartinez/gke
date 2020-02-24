cb2f2c39d337f6db78fe7a7e329c76b86f3d58b1
Oscar González <oscar.gonzalezmartinez@gmail.com>
2020-02-24 19:54:36 +0100

    Update HelloController.java

--- src/main/java/org/ogm/gke/gkedemo/HelloController.java
+++ src/main/java/org/ogm/gke/gkedemo/HelloController.java
@@ -1,5 +1,9 @@
 package org.ogm.gke.gkedemo;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +13,10 @@ import io.micrometer.core.instrument.Metrics;
 @RestController("/")
 public class HelloController {
 
+	@RequestMapping("/")
+	public ResponseEntity<String> initial() {
+		return new ResponseEntity<>("now is " + new Date(), HttpStatus.OK);
+	}
 	
 	@RequestMapping("/hola")
 	public String sayHello(@RequestParam(value="name") String name) {
